f49f64a316cadf5370d048898601faaef4c2b117
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-13 10:59:11 -0400

    flirt version 1

---
+++ r2013-03-12-flirt/app/controllers/subscribers_controller.rb
@@ -0,0 +1,9 @@
+class SubscribersController < ApplicationController
+  def new
+    @subscriber = Subscriber.new
+  end
+
+  def create
+    Subscriber.create(params[:subscriber])
+  end
+end
---
+++ r2013-03-12-flirt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R20130312Flirt::Application.config.secret_token = '528bc8d71d4f01c24531c221946db58654f7d9811483d6dbf0f77b36eaa8a4c66081494b4c13d95f4a61421b0b1568ac76bc7b8ca7390ede76862f7a69ca4a88'
